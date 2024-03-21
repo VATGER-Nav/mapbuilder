d250ba127feee264a8a7fa0849129c3f6cd3a99c
Franz Pletz <fpletz@fnordicwalking.de>
2024-03-21 15:52:54 +0100

    flake: reformat with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -12,46 +12,62 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-  flake-parts.lib.mkFlake
-    { inherit inputs; }
-    ({ inputs, lib, withSystem, ... }:
-    {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        inputs,
+        lib,
+        withSystem,
+        ...
+      }:
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
 
-      perSystem = { pkgs, system, self', ... }:
-      let
-        poetry2nix = import inputs.poetry2nix {inherit pkgs;};
-        overrides = poetry2nix.overrides.withDefaults (final: prev: {
-          pygeodesy = prev.pygeodesy.overridePythonAttrs (old: {
-            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.setuptools];
-          });
-        });
-        devEnv = poetry2nix.mkPoetryEnv {
-          inherit overrides;
-          projectDir = ./.;
-          editablePackageSources = {
-            mapbuilder = ./.;
+        perSystem =
+          {
+            pkgs,
+            system,
+            self',
+            ...
+          }:
+          let
+            poetry2nix = import inputs.poetry2nix { inherit pkgs; };
+            overrides = poetry2nix.overrides.withDefaults (
+              final: prev: {
+                pygeodesy = prev.pygeodesy.overridePythonAttrs (old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.setuptools ];
+                });
+              }
+            );
+            devEnv = poetry2nix.mkPoetryEnv {
+              inherit overrides;
+              projectDir = ./.;
+              editablePackageSources = {
+                mapbuilder = ./.;
+              };
+            };
+          in
+          {
+            packages.default = poetry2nix.mkPoetryApplication {
+              inherit overrides;
+              projectDir = ./.;
+            };
+            devShells.default = devEnv.env.overrideAttrs (attrs: {
+              nativeBuildInputs = attrs.nativeBuildInputs ++ [
+                pkgs.poetry
+                pkgs.nil
+                pkgs.pyright
+              ];
+              shellHook = ''
+                export PYTHONPATH=${devEnv}/${devEnv.sitePackages}
+              '';
+            });
+            formatter = pkgs.nixfmt-rfc-style;
           };
-        };
-      in {
-        packages.default = poetry2nix.mkPoetryApplication {
-          inherit overrides;
-          projectDir = ./.;
-        };
-        devShells.default = devEnv.env.overrideAttrs (attrs: {
-          nativeBuildInputs = attrs.nativeBuildInputs ++ [
-            pkgs.poetry
-            pkgs.nil
-            pkgs.pyright
-          ];
-          shellHook = ''
-            export PYTHONPATH=${devEnv}/${devEnv.sitePackages}
-          '';
-        });
-      };
-    });
+      }
+    );
 }
